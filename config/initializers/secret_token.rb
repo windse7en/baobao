2fdfec57550f1a3de3a41e5107b7c7d40aa3d027
windse7en <windse7en@gmail.com>
2015-06-16 16:27:51 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Baobao::Application.config.secret_key_base = '6f43f07111255fdf34450f11323ee9ae6a6ef34078d727de6f16d13e96b430b2f8856bb4d6e8098e69c011c94e54cff7c84c7846fb512577dd3f05c7d61c7907'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Baobao::Application.config.session_store :cookie_store, key: '_baobao_session'
