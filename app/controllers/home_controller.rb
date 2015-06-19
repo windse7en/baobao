01ffc0bf28297c7d50efa9d27155de378ebbdeb6
windse7en <windse7en@gmail.com>
2015-06-19 16:33:59 -0400

    add the post_calculate for wife

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end 
+
+  def post_calculate
+  end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Baobao::Application.routes.draw do
   root 'home#index'
+  get '/post_calculate' => 'home#post_calculate'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
